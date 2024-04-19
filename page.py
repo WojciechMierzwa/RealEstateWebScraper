ea45398df0ad1698f55c5372405b35162ba7b651
WojciechMierzwa <96776516+WojciechMierzwa@users.noreply.github.com>
2024-04-19 23:46:07 +0200

    Add files via upload

--- page.py
+++ page.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -16,9 +16,36 @@ def index():
 @app.route('/scrape')
 def scrape():
     collection = connect_to_mongodb()
-    results = collection.find()  # Pobierz wszystkie dokumenty z kolekcji
+    # Get filtering parameters from request
+    min_price = request.args.get('min_price')
+    max_price = request.args.get('max_price')
+    min_area = request.args.get('min_area')
+    max_area = request.args.get('max_area')
+    min_rooms = request.args.get('min_rooms')
+    max_rooms = request.args.get('max_rooms')
+    market = request.args.get('market')
+    
+    # Construct query based on provided parameters
+    query = {}
+    if min_price:
+        query['Cena'] = {'$gte': int(min_price)}
+    if max_price:
+        query.setdefault('Cena', {}).update({'$lte': int(max_price)})
+    if min_area:
+        query['Powierzchnia'] = {'$gte': int(min_area)}
+    if max_area:
+        query.setdefault('Powierzchnia', {}).update({'$lte': int(max_area)})
+    if min_rooms:
+        query['Liczba_pokoi'] = {'$gte': int(min_rooms)}
+    if max_rooms:
+        query.setdefault('Liczba_pokoi', {}).update({'$lte': int(max_rooms)})
+    if market:
+        if market == 'pierwotny' or market == 'wtórny':
+            query['Rynek'] = market
+    
+    # Query MongoDB with the constructed query
+    results = collection.find(query)
     return render_template('scrape_results.html', results=results)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
