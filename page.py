5b332826b43cc7499a9ba6e3cfad9cd511c2e989
WojciechMierzwa <96776516+WojciechMierzwa@users.noreply.github.com>
2024-04-22 21:39:42 +0200

    Add files via upload

--- page.py
+++ page.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING, DESCENDING
+import re
 
 app = Flask(__name__)
 
@@ -9,14 +10,16 @@ def connect_to_mongodb():
     collection = db['Mieszkania']
     return collection
 
+def clean_and_extract_digits(text):
+    # Usuń spacje i inne znaki niebędące cyframi
+    digits = re.sub(r'[^\d,]', '', text)
+    # Zamień przecinki na kropki dla poprawy formatu zmiennoprzecinkowego
+    digits = digits.replace(',', '.')
+    return digits
+
 @app.route('/')
 def index():
     return render_template('index.html')
-def remove_non_numeric(text):
-    numeric_part = re.search(r'\d[\d\s]*', text)
-    if numeric_part:
-        return float(numeric_part.group().replace(" ", ""))
-    return None
 
 @app.route('/scrape')
 def scrape():
@@ -29,19 +32,45 @@ def scrape():
     max_rooms = request.args.get('max_rooms')
     market = request.args.get('market')
     floor = request.args.get('floor')
+    sort_date = request.args.get('sort_date')
+    sort_price = request.args.get('sort_price')
+    sort_area = request.args.get('sort_area')
     
     query = {}
+    if min_price:
+        query['Cena:'] = {'$gte': float(clean_and_extract_digits(min_price))}
+    if max_price:
+        query.setdefault('Cena:', {}).update({'$lte': float(clean_and_extract_digits(max_price))})
+    if min_area:
+        query['Powierzchnia:'] = {'$gte': float(clean_and_extract_digits(min_area))}
+    if max_area:
+        query.setdefault('Powierzchnia:', {}).update({'$lte': float(clean_and_extract_digits(max_area))})
     if floor:
         query.setdefault('Piętro:', {}).update({'$eq': floor})
     if min_rooms:
         query['Liczba pokoi:'] = {'$gte': min_rooms}
     if max_rooms:
         query.setdefault('Liczba pokoi:', {}).update({'$lte': max_rooms})
+
     if market:
         if market == 'pierwotny' or market == 'wtórny':
             query['Rynek:'] = market
     
-    results = collection.find(query)
+    if sort_date:
+        sort_direction = ASCENDING if sort_date == 'asc' else DESCENDING
+        results = collection.find(query).sort('Data ogłoszenia', sort_direction)
+    elif sort_price:
+        sort_direction = ASCENDING if sort_price == 'asc' else DESCENDING
+        results = collection.find(query).sort('Cena:', sort_direction)
+        # Konwertujmy cenę na float przed sortowaniem
+        results = sorted(results, key=lambda x: float(clean_and_extract_digits(x['Cena:'])), reverse=(sort_price == 'desc'))
+    elif sort_area:
+        sort_direction = ASCENDING if sort_area == 'asc' else DESCENDING
+        results = collection.find(query).sort('Powierzchnia:', sort_direction)
+        # Konwertujmy powierzchnię na float przed sortowaniem
+        results = sorted(results, key=lambda x: float(clean_and_extract_digits(x['Powierzchnia:'])), reverse=(sort_area == 'desc'))
+    else:
+        results = collection.find(query)
 
     return render_template('scrape_results.html', results=results)
 
